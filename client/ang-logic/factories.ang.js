b87873979ee2893bcb54654f667b30235165d86b
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-08-31 17:18:23 -0500

    adds directions to user page

--- client/ang-logic/factories.ang.js
+++ client/ang-logic/factories.ang.js
@@ -1,1 +1,6 @@
-var factory = angular.module('app.factories',['ngRoute']);
+var app = angular.module('app.factories', []);
+app.factory('serviceId', function() {
+  var shinyNewServiceInstance;
+  // factory function body that constructs shinyNewServiceInstance
+  return shinyNewServiceInstance;
+});
